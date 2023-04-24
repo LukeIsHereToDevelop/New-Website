c3d13ec2799bec8cbe078595019483ffd58eb25e
Luca <43251906+LukeIsHereToDevelop@users.noreply.github.com>
2023-04-24 17:15:46 +0200

    🚀 Now using Lanyard's WebSocket

--- script.js
+++ script.js
@@ -10,14 +10,13 @@ window.addEventListener("load", () => {
 });
 
 // Discord Status
-const updatePresence = (async () => {
-    const request = await fetch("https://api.lanyard.rest/v1/users/1095455197121237103");
-    const json = await request.json();
-    
-    if (json.success) {
+lanyard({
+    userId: "1095455197121237103",
+    socket: true,
+    onPresenceUpdate: (json) => {
         const status = document.getElementsByClassName("status")[0];
 
-        switch (json.data.discord_status) {
+        switch (json.discord_status) {
             case "online":
                 status.classList.replace(status.classList.item(1), "online");
                 break;
@@ -34,29 +33,30 @@ const updatePresence = (async () => {
 
         const presence = document.getElementsByClassName("presence")[0];
 
-        if (json.data.spotify) {
+        if (json.spotify) {
             let image = document.getElementsByClassName("image")[0];
             let name = document.getElementsByClassName("name")[0];
 
-            presence.onclick = () => window.open(`https://open.spotify.com/track/${json.data.spotify.track_id}`);
-            image.src = json.data.spotify.album_art_url;
-            name.innerHTML = `${json.data.spotify.song.length > 12 ? json.data.spotify.song.slice(0, 9) + "..." : json.data.spotify.song}<br>${json.data.spotify.artist.length > 12 ? json.data.spotify.artist.slice(0, 9) + "..." : json.data.spotify.artist}`;
+            presence.onclick = () => window.open(`https://open.spotify.com/track/${json.spotify.track_id}`);
+            image.src = json.spotify.album_art_url;
+            name.innerHTML = `${json.spotify.song.length > 12 ? json.spotify.song.slice(0, 9) + "..." : json.spotify.song}<br>${json.spotify.artist.length > 12 ? json.spotify.artist.slice(0, 9) + "..." : json.spotify.artist}`;
         }
-        else if (json.data.activities.find((el) => el.application_id === "810516608442695700")) {
-            const activity = json.data.activities.find((el) => el.application_id === "810516608442695700");
+        else if (json.activities.find((el) => el.application_id === "810516608442695700")) {
+            const activity = json.activities.find((el) => el.application_id === "810516608442695700");
+
+            console.log(activity);
             
             let image = document.getElementsByClassName("image")[0];
             let name = document.getElementsByClassName("name")[0];
 
+            if (activity.details.includes("Not in a file")) name.innerHTML = `VSCode<br>No file`;
+            else name.innerHTML = `VSCode<br>${(activity.state.split(" ")[2]).split(":")[0]}`;
+
             image.src = `https://${activity.assets.large_image.split("https/")[1]}`;
-            name.innerHTML = `VSCode<br>${(activity.state.split(" ")[2]).split(":")[0]}`;
         }
         else {
             let presence = document.getElementsByClassName("presence")[0];
             presence.style.display = "none";
         }
-    };
+    }
 });
-
-updatePresence();
-setInterval(updatePresence, 15_000);
